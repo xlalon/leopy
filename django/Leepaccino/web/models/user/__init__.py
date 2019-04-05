e5700ab7566dc2510963de2f697389ea9244b00c
leo <meiying210@163.com>
2019-04-05 21:02:53 +0800

    bug fix

---
+++ django/Leepaccino/web/models/user/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+from .user_info import User
+
+
+__all__ = ['User']
---
+++ django/Leepaccino/web/views/user/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.urls import path
+from .info import UserInfoView
+from .register import UserRegisterHandler
+from .common_login import CommonLoginHandler
+
+
+urls_user = [
+    path('user/info', UserInfoView.as_view(), name='user_info'),
+    path('user/register', UserRegisterHandler.as_view(), name='user_register'),
+    path('user/common_login', CommonLoginHandler.as_view(), name='common_login'),
+]
